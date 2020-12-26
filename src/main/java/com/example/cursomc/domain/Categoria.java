ccb051ba2e06e603b3a5e6e95c0e2b534e56bb93
nauam <nauamvictor@outlook.com>
2020-12-26 13:59:27 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/example/cursomc/domain/Categoria.java
+++ src/main/java/com/example/cursomc/domain/Categoria.java
@@ -2,9 +2,17 @@ package com.example.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
