1af191262d20c1057e6ff73d4585a92f4701ec6e
nauam <nauamvictor@outlook.com>
2020-12-27 19:02:34 -0300

    DELETE

--- src/main/java/com/example/cursomc/Service/CategoriaService.java
+++ src/main/java/com/example/cursomc/Service/CategoriaService.java
@@ -3,8 +3,10 @@ package com.example.cursomc.Service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import com.example.cursomc.Service.exceptions.DataIntegrityException;
 import com.example.cursomc.Service.exceptions.ObjectNotFoundException;
 import com.example.cursomc.domain.Categoria;
 import com.example.cursomc.repositories.CategoriaRepository;
@@ -15,18 +17,29 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria find(Integer id){
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
-	public Categoria insert(Categoria obj){
+
+	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
-	public Categoria update(Categoria obj){
+
+	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 }
