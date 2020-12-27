1d700c7e3ab653cb08a18371ed01cb9b387edc19
nauam <nauamvictor@outlook.com>
2020-12-27 13:11:09 -0300

    Endpoint /cliente/{id} disponivel

---
+++ src/main/java/com/example/cursomc/Service/ClienteService.java
@@ -0,0 +1,22 @@
+package com.example.cursomc.Service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cursomc.Service.exceptions.ObjectNotFoundException;
+import com.example.cursomc.domain.Cliente;
+import com.example.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id){
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
